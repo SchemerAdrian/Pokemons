0bec4fea84ff4c07fa1b276dcfd79213ee741946
SchemerAdrian <angelpill2706@gmail.com>
2025-05-14 23:47:54 -0300

    coisas que mudei

---
+++ Gen1.js
@@ -0,0 +1,11 @@
+const pokemons = [
+    {numero: "001", nome: "Bulbasaur" , tipo: "Planta" , descricao: "Uma semente estranha foi plantada em suas costas ao nascer. A planta brota e cresce junto com este pokemon."}
+
+];
+
+document.getElementById("proximo").addEventListener("click",function() {
+    alert("aaaaaaa");
+});
+
+
+
